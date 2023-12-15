269a70da85927e498d83ad483fe59499c7b67f82
Martin Flasar <christian.flasar@gmail.com>
2023-12-15 21:10:09 +0100

    Added FamilyFameDropRate. Reworked and refactored Tax and Region. Changed styles and functionality.

--- client/src/components/ui/pages/UserControlPanel/Profile/ProfileSettings/ProfileSettings.jsx
+++ client/src/components/ui/pages/UserControlPanel/Profile/ProfileSettings/ProfileSettings.jsx
@@ -31,11 +31,18 @@ const ProfileSettings = () => {
   }
 
   const handleValuePackChange = (e) => {
-    dispatch({ type: 'PROFILE_SETTINGS_INPUT_CHANGE', payload: { name: e.target.name, value: e.target.checked } })
+    const invertState = !state.valuePack
+    dispatch({ type: 'PROFILE_SETTINGS_INPUT_CHANGE', payload: { name: e.target.getAttribute('name'), value: invertState } })
   }
 
   const handleMerchantRingChange = (e) => {
-    dispatch({ type: 'PROFILE_SETTINGS_INPUT_CHANGE', payload: { name: e.target.name, value: e.target.checked } })
+    const invertState = !state.merchantRing
+    dispatch({ type: 'PROFILE_SETTINGS_INPUT_CHANGE', payload: { name: e.target.getAttribute('name'), value: invertState } })
+  }
+
+  const handleFamilyFameDropRate = (e) => {
+    const invertState = !state.familyFameDropRate
+    dispatch({ type: 'PROFILE_SETTINGS_INPUT_CHANGE', payload: { name: e.target.getAttribute('name'), value: invertState } })
   }
 
   const handleFamilyFameChange = (e) => {
@@ -50,6 +57,13 @@ const ProfileSettings = () => {
         tempFamilyFame = 0
       }
     }
+
+    if (tempFamilyFame >= 7000 && !state.familyFameDropRate) {
+      dispatch({ type: 'PROFILE_SETTINGS_INPUT_CHANGE', payload: { name: 'familyFameDropRate', value: true } })
+    } else if (state.familyFameDropRate && tempFamilyFame < 7000) {
+      dispatch({ type: 'PROFILE_SETTINGS_INPUT_CHANGE', payload: { name: 'familyFameDropRate', value: false } })
+    }
+
     dispatch({ type: 'PROFILE_SETTINGS_INPUT_CHANGE', payload: { name: e.target.name, value: tempFamilyFame } })
   }
 
@@ -59,6 +73,7 @@ const ProfileSettings = () => {
     const valuePack = state.valuePack
     const merchantRing = state.merchantRing
     const familyFame = state.familyFame
+    const familyFameDropRate = state.familyFameDropRate
 
     const response = await authorizedFetch('api/user/setusersettingsdata', {
       method: 'POST',
@@ -69,7 +84,8 @@ const ProfileSettings = () => {
         regionServer,
         valuePack,
         merchantRing,
-        familyFame
+        familyFame,
+        familyFameDropRate
       })
     })
     if (response.ok) {
@@ -86,23 +102,60 @@ const ProfileSettings = () => {
               ? (
             <form aria-label='profileSettings-container-form'>
                 <div className='profileSettings-container-form-inputlabel'>
-                  <label htmlFor='regionServer'>Server/Region:</label>
-                  <input type='text' id='regionServer' name='regionServer' placeholder='Server/Region' value={state.regionServer} onChange={handleRegionServerChange} />
-                  <label htmlFor='grindingPreference'>Grinding preference:</label>
-                  <div className='profileSettings-container-form-inputlabel-grindingPreference'>
-                      <label htmlFor='valuePack'>Value Pack</label>
-                      <input type='checkbox' id='valuePack' name='valuePack' checked={state.valuePack} onChange={handleValuePackChange}/>
-                      <label htmlFor='merchantRing'>Merchant Ring</label>
-                      <input type='checkbox' id='merchantRing' name='merchantRing' checked={state.merchantRing} onChange={handleMerchantRingChange}/>
-                      <label htmlFor='familyFame'>Family Fame</label>
-                      <input type='text' id='familyFame' name='familyFame' pattern='[0-9.]+' value={state.familyFame} onChange={handleFamilyFameChange}/>
+                  <h2>Region and Localization</h2>
+                  <div className='delimiter' />
+                  <div className='regionLocalization'>
+                    <div className='regionLocalization-region'>
+                      <label htmlFor='region'>Region:</label>
+                      {/* add dropdown menu for region and add localization */}
+                      <input type='text' id='region' name='region' placeholder='Region' value={state.regionServer} onChange={handleRegionServerChange} />
+                    </div>
                   </div>
-                  <label htmlFor='tax'>Tax</label>
-                <div className='profileSettings-container-form-inputlabel-tax'>
+                  <h2 htmlFor='grindingPreference'>Tax</h2>
+                  <div className='delimiter' />
+                  <div className='profileSettings-container-form-inputlabel-grindingPreference'>
+                    <div className='leftSide'>
+                      <div className='holderBox'>
+                        <label htmlFor='valuePack'>Value Pack</label>
+                        <div className='buttonHolder' id='valuePack' name='valuePack' onClick={(e) => handleValuePackChange(e)}>
+                          <div className={state.valuePack ? 'buttonActive' : ''} name='valuePack'>Yes</div>
+                          <div className={!state.valuePack ? 'buttonInactive' : ''} name='valuePack'>No</div>
+                        </div>
+                      </div>
+
+                      <div className='holderBox'>
+                        <label htmlFor='merchantRing'>Merchant Ring</label>
+                        <div className='buttonHolder' id='merchantRing' name='merchantRing' onClick={(e) => handleMerchantRingChange(e)}>
+                          <div className={state.merchantRing ? 'buttonActive' : ''} name='merchantRing'>Yes</div>
+                          <div className={!state.merchantRing ? 'buttonInactive' : ''} name='merchantRing'>No</div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className='tax'>
+                      <label htmlFor='tax'>Tax</label>
+                      <div className='profileSettings-container-form-inputlabel-tax'>
                         <input type='text' className='profileSettings-container-form-inputlabel-tax-total' disabled value={((1 + state.tax) * 100).toFixed(2) + '%'} />
                         <div className='profileSettings-container-form-inputlabel-tax-deliminator'>|</div>
                         <input type='text' className='profileSettings-container-form-inputlabel-tax-taxed' disabled value={state.tax * 100 + '%'} />
-                </div>
+                      </div>
+                    </div>
+
+                    <div className='rightSide'>
+                      <div className='holderBox'>
+                      <label htmlFor='familyFame'>Family Fame</label>
+                        <input type='text' id='familyFame' name='familyFame' pattern='[0-9.]+' value={state.familyFame} onChange={handleFamilyFameChange}/>
+                      </div>
+
+                      <div className='holderBox'>
+                        <label htmlFor='familyFameDropRate'>Family fame 7000+</label>
+                        <div className='buttonHolder' id='familyFameDropRate' name='familyFameDropRate' onClick={(e) => handleFamilyFameDropRate(e)}>
+                          <div className={state.familyFameDropRate ? 'buttonActive' : ''} name='familyFameDropRate'>Yes</div>
+                          <div className={!state.familyFameDropRate ? 'buttonInactive' : ''} name='familyFameDropRate'>No</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <button id='profileSettingsSaveButton' name='profileSettingsSaveButton' onClick={SaveSettingsData}>Save</button>
             </form>)
